2937382aae8b6c59cf4b7697e9e0e5ee0d1f38c3
dimonstrik <dimonstrik@me.com>
2022-05-30 10:55:02 +0300

    created

--- src/vv.js
+++ src/vv.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './m.svg';
+import mp3 from "./audio/s.mp3"
+import './vv.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> 
+          Весело <code>и</code> вкусно?
         </p>
+        <div>
+          <audio src={mp3} controls autoPlay loop/>
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D1%88%D0%B8%D0%BD%D0%B3"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I'm lovin' it
         </a>
       </header>
     </div>
