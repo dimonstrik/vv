495ec8296906f38b8e96abf020c27bc712cc33f8
dimonstrik <dimonstrik88@gmail.com>
2022-05-31 13:44:15 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './vv';
 
-test('renders learn react link', () => {
+test('renders slogan link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I'm lovin' it/i);
   expect(linkElement).toBeInTheDocument();
 });
